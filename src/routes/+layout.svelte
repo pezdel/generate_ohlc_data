85bd1974058c9fcd7cd7de80d5f612f62c6ee276
pezdel <adam.pezdel@gmail.com>
2023-09-02 18:34:47 -0500

    added anyltics

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -4,6 +4,10 @@
 	// Floating UI for Popups
 	import { computePosition, autoUpdate, flip, shift, offset, arrow } from '@floating-ui/dom';
 	import { storePopup } from '@skeletonlabs/skeleton';
+	import { dev } from '$app/environment';
+	import { inject } from '@vercel/analytics';
+
+	inject({ mode: dev ? 'development' : 'production' });
 	storePopup.set({ computePosition, autoUpdate, flip, shift, offset, arrow });
 </script>
 
